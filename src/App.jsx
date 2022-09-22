cd32bb9d9b183ae745a3f6aff53dfc4bbaea344e
mediolano <dawid.milaniuk@gmail.com>
2022-09-22 06:35:06 +0200

    Fix page structure

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,28 @@ import {
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>Navbar</div>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar></Navbar>
+      </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>Hero </div>
+      <div className={`${styles.boxWidth}`}>
+        <Hero></Hero>
+      </div>
     </div>
 
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>Rest of the components</div>
+      <div className={`${styles.boxWidth}`}>
+        <Stats></Stats>
+        <Business></Business>
+        <Billing></Billing>
+        <CardDeal></CardDeal>
+        <Testimonials></Testimonials>
+        <Clients></Clients>
+        <CTA></CTA>
+        <Footer></Footer>
+      </div>
     </div>
   </div>
 );
