9d223ca95a3885a72d611269007eaba3f5a068f6
mediolano <dawid.milaniuk@gmail.com>
2022-09-23 22:55:15 +0200

    Add left side of business component

--- src/components/Bussiness.jsx
+++ src/components/Bussiness.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const Bussiness = () => {
-  return <div>Bussiness</div>;
-};
+const Bussiness = () => (
+  <section id="features" className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        You do the bussiness,
+        <br className="sm:block hidden" /> we'll handle this.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        With the right credit card, you can improve your financial life by
+        building credit, earining rewards and saving money. But with hundreds of
+        credit cards on the market.
+      </p>
+    </div>
+  </section>
+);
 
 export default Bussiness;
