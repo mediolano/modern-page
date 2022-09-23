6e31e2450a352d629c9a2fb50d8725ceb2c16885
mediolano <dawid.milaniuk@gmail.com>
2022-09-24 01:04:36 +0200

    Round button corners

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+ćimport React from "react";
 
 const Button = ({ styles }) => (
   <button
     type="button"
-    className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] rounded text-primary outline-none ${styles}`}
+    className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] rounded-[10px] text-primary outline-none ${styles}`}
   >
     Get Started
   </button>
