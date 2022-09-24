208cca6f2957c79692009d510e2b684fae3cccb1
mediolano <dawid.milaniuk@gmail.com>
2022-09-24 02:18:45 +0200

    Add CTA component

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import styles from "../style";
+import Button from "./Button";
 
-const CTA = () => {
-  return <div>CTA</div>;
-};
+const CTA = () => (
+  <section
+    className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow bg-black-gradient-2`}
+  >
+    <div className="flex-1 flex flex-col">
+      <h2 className={styles.heading2}>Let's try our service now!</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto pariatur
+        a vero laborum animi modi, atque quisquam esse hic placeat.
+      </p>
+    </div>
+    <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+      <Button />
+    </div>
+  </section>
+);
 
 export default CTA;
