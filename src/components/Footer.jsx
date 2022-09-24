73d1856d96093248ea86b0da2204f6c97a535bd8
mediolano <dawid.milaniuk@gmail.com>
2022-09-24 02:58:52 +0200

    Fix line color in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -40,7 +40,7 @@ const Footer = () => (
         ))}
       </div>
     </div>
-    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3r45]">
+    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] text-white leading-[27px]">
         © Copyright 2022 mediolano. All rights Reserved.
       </p>
